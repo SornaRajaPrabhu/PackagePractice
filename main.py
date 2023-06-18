60278e0197b2b820727d2bdbd030337c28ed114b
Sorna Raja Prabhu <105154789+SornaRajaPrabhu@users.noreply.github.com>
2023-06-18 19:47:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import utils
 from flask import Flask
+import sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def startAction():
-    obj = utils.GetLength.GetLength("SornaRajaPrabhu")
-    return str(obj.length())
+    #obj = utils.GetLength.GetLength("SornaRajaPrabhu")
+    return sys.path
+    #return str(obj.length())
 
 if __name__ == '__main__':
     app.run()
