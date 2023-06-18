d1fbaabf16d3d82e2827dbdaab3763bc6181b692
Sorna Raja Prabhu <105154789+SornaRajaPrabhu@users.noreply.github.com>
2023-06-18 19:24:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from utils import GetLength
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def startAction():
+    obj = GetLength.GetLength("SornaRajaPrabhu")
+    return str(obj.length())
 
 if __name__ == '__main__':
-    obj = GetLength.GetLength("SornaRajaPrabhu")
-    print(obj.length())
+    app.run()
